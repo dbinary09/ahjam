18a89eba88eaaa37fd82713e7bf0c3a3651d7369
dbinary09 <Desmund.Yeng@MYNB172.fusionex.local>
2015-03-10 19:17:43 +0800

    !

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
+
+var port = process.env.PORT || 5000;
+app.use(express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT || 5000);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -14,8 +20,8 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:5000');
 });
 
 // broadcast to all participants
