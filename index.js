d03e36c0f959ded2b2186801cdb81f87e728c4c3
dbinary09 <Desmund.Yeng@MYNB172.fusionex.local>
2015-03-10 19:20:35 +0800

    !

--- index.js
+++ index.js
@@ -11,22 +11,3 @@ app.listen(process.env.PORT || 5000);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
-io.on('connection', function(socket){
-  io.emit('chat message', "a user is connected");
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-    io.emit('chat message', "a user is disconnected");
-  });
-});
-
-http.listen(port, function(){
-  console.log('listening on *:5000');
-});
-
-// broadcast to all participants
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  })
-});
