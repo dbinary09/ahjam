0f308028b370192cc422b7061763fb5bb710cb0f
dbinary09 <Desmund.Yeng@MYNB172.fusionex.local>
2015-03-11 15:55:34 +0800

    !

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.listen(process.env.PORT || 5000);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 });
 
 io.on('connection', function(socket){
